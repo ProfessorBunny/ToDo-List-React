15ba5857d3d384a27f54f1d674f05f45f301920b
200~ProfessorBunny <nikunjraghav24@gmail.com>
2023-07-07 09:01:26 +0100

    From Component created

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
-  return <div>Form</div>;
+  const [newItem, setNewItem] = useState("");
+  const changeHandler = (e) => {
+    setNewItem(e.target.value);
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(newItem);
+    setNewItem("");
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <h4>To Do List</h4>
+      <div className="form-control">
+        <input
+          type="text"
+          className="form-input"
+          value={newItem}
+          onChange={changeHandler}
+        />
+        <button className="btn" type="submit">
+          Add New Task
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default Form;
