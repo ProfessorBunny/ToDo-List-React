526528032764744cda85a106049a7ae7b0ec0eed
ProfessorBuny <nikunjraghav24@gmail.com>
2025-04-11 17:55:37 +0100

    success msg updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
-import Form from "./Form";
-import { nanoid } from "nanoid";
-import Tasks from "./Tasks";
-import { ToastContainer, toast } from "react-toastify";
+import { useState } from 'react'
+import Form from './Form'
+import { nanoid } from 'nanoid'
+import Tasks from './Tasks'
+import { ToastContainer, toast } from 'react-toastify'
 
 const App = () => {
   const setLocalStorage = (tasks) => {
-    localStorage.setItem("taskList", JSON.stringify(tasks));
-  };
+    localStorage.setItem('taskList', JSON.stringify(tasks))
+  }
   // const getLocalStorage = (tasks) => {
   //   let taskList = localStorage.getItem("taskList");
   //   if (taskList) {
@@ -19,49 +19,49 @@ const App = () => {
   // };
   // const [tasks, setTasks] = useState(getLocalStorage);
 
-  const taskList = JSON.parse(localStorage.getItem("taskList") || "[]");
+  const taskList = JSON.parse(localStorage.getItem('taskList') || '[]')
 
-  const [tasks, setTasks] = useState(taskList);
+  const [tasks, setTasks] = useState(taskList)
 
   const addTasks = (taskName) => {
     const newTask = {
       name: taskName,
       completed: false,
       id: nanoid(),
-    };
-    const newTasks = [...tasks, newTask];
-    setTasks(newTasks);
-    setLocalStorage(newTasks);
-    toast.success("Task successfully added");
-  };
+    }
+    const newTasks = [...tasks, newTask]
+    setTasks(newTasks)
+    setLocalStorage(newTasks)
+    toast.success('Task successfully added to the list')
+  }
   const editTask = (taskId) => {
     const newTasks = tasks.map((task) => {
       if (task.id === taskId) {
-        const editedTask = { ...task, completed: !task.completed };
-        return editedTask;
+        const editedTask = { ...task, completed: !task.completed }
+        return editedTask
       }
-      return task;
-    });
+      return task
+    })
 
-    setTasks(newTasks);
-    setLocalStorage(newTasks);
-    toast.success("Task status updated");
-  };
+    setTasks(newTasks)
+    setLocalStorage(newTasks)
+    toast.success('Task status updated')
+  }
 
   const removeTask = (taskId) => {
-    const remainingTasks = tasks.filter((task) => task.id !== taskId);
+    const remainingTasks = tasks.filter((task) => task.id !== taskId)
 
-    setTasks(remainingTasks);
-    setLocalStorage(remainingTasks);
-    toast.success("Task successfully removed");
-  };
+    setTasks(remainingTasks)
+    setLocalStorage(remainingTasks)
+    toast.success('Task successfully ')
+  }
   return (
     <section className="section-center">
       <ToastContainer position="top-center" />
       <Form addTasks={addTasks} />
       <Tasks tasks={tasks} removeTask={removeTask} editTask={editTask} />
     </section>
-  );
-};
+  )
+}
 
-export default App;
+export default App
